83ba1af1a20d3560f255d105ccf81c53ba9259ab
Sumalm <sumalm.1si23mc053@gmail.com>
2026-01-17 15:44:00 +0530

    Configure CORS using env variables for frontend and admin

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,14 +1,21 @@
-const allowedOrigins = ['http://localhost:5173','http://localhost:5174']
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_URL,
+  'http://localhost:5173',
+  'http://localhost:5174'
+];
 
-const corsOptions ={
-    origin: (origin,callback)=>{
-        if(allowedOrigins.indexOf(origin)!== -1 || !origin){
-            callback(null,true)
-        }else{
-            callback(new Error('Not allowed by CORS'))
-        }
-   },
-    credentials:true,  //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: (origin, callback) => {
+    // allow requests with no origin (Postman, mobile apps)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
 module.exports = corsOptions;
